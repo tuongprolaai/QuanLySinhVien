127c37b5f0119f31aa04f0936a01f67d5e143480
tranq <tranq@DESKTOP-M9FFJ0C>
2025-09-15 15:31:23 +0700

    Keep my code

--- be/BE-QLSV/BE-QLSV/obj/Debug/net8.0/BE-QLSV.AssemblyInfo.cs
+++ be/BE-QLSV/BE-QLSV/obj/Debug/net8.0/BE-QLSV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE-QLSV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+888322f499e697952a9832b10fdb4b7f53b8ede1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b34458df98ce3e8887bb47a5c886154a0ac9ab7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE-QLSV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE-QLSV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
